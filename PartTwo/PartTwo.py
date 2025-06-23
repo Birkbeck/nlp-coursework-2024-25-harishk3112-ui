ce21efde480cee60f24abd66d8b685d4bee89dfd
Harish Krishnakumar <harishk3112@gmail.com>
2025-06-23 16:20:20 +0100

    Q2(b): Applied TF-IDF vectorization and completed 80/20 stratified train-test split.

--- PartTwo/PartTwo.py
+++ PartTwo/PartTwo.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+# Q2(a) : Data Preprocessing
 
 # Load the dataset
 df = pd.read_csv("PartTwo/hansard40000.csv")
@@ -18,3 +23,24 @@ df = df[df["speech"].str.len() >= 1000]
 
 # Print the final shape
 print("Filtered DataFrame shape:", df.shape)
+
+# Q2(b) : Train/Test  Data Split
+
+# Create a TF-IDF Vectorizer
+vectorizer = TfidfVectorizer(stop_words='english', max_features=3000)
+
+# Apply vectorizer to the 'speech' column
+X = vectorizer.fit_transform(df["speech"])
+
+# Extract labels
+y = df["party"]
+
+# Split data using stratified sampling with seed 26
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=26
+)
+
+# Print result sizes
+print(" TF-IDF shape:", X.shape)
+print("Train set size:", X_train.shape[0])
+print("Test set size:", X_test.shape[0])
